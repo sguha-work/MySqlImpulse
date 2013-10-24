b5f5abe03c892ff25a597c8531dc9516fc106be0
sguha-cn <sguha.cn@gmail.com>
2013-10-24 12:43:51 +0530

    24.10.2013 12:43pm

--- system/Cache.php
+++ system/Cache.php
@@ -1,19 +1,28 @@
 <?php
-//THIS CLASS IS RESPONSIBLE TO WRITE DATA TO CACHE FILE AND 
-//GETTING VALUES FROM CASCHE FILE
+//THIS CLASS IS RESPONSIBLE TO WRITE DATA TO CACHE AND 
+//GETTING VALUES FROM CASCHE 
 class Cache
 {
-	/*
-	$fileName SHOULD BE THE CAHCE FILE NAME,AND $data WILL BE 
-	THE ARRAY HOLDING THE RETURNED ROWS FROM DATABASE
-	*/
-	function writeToCache($fileName,$data)
-	{
-	  $serializeData=serialize($data);	
-      $result=file_put_contents($fileName,$serializeData);
-      return $result;
+	private static $CacheArray=array();
+	private static $cache_object=null;
+	private function __construct() {
+
 	}
-	function getFromCache($fileName)
+	public static getCacheObject() {
+		if( $cache_object == null ) {
+			self :: $cache_object = new Cache();
+		}
+		return $cache_object;
+	}
+	public function writeToCache( $index, $data ) {
+	  $serializeData = serialize($data);	
+      if( $index == null ) {
+      	die('Error:No Index Specified.Unable to write cahce');
+      }
+      self :: $CacheArray[ $index ] = serialize( $data );
+      return true;
+	}
+	public function getFromCache($fileName)
 	{
       $serializeData=file_get_contents($fileName);
       $data=array();

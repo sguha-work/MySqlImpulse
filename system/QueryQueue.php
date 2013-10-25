4367e6de470c529cee6eb4898cf8051b7e2d617b
sguha-cn <sguha.cn@gmail.com>
2013-10-25 19:43:43 +0530

    25.10.2013 7:43PM

--- system/QueryQueue.php
+++ system/QueryQueue.php
@@ -4,13 +4,16 @@
 *the query queue is maintained by the Cache MAnage class
 */
 class QueryQueue {
-	private $QuesueArray = array();
-	private $QueueSize   = 0;
-	private $front       = -1;
-	private $rear        = -1
+	private $QuesueArray = array();//Main Query array
+	private $QueueSize   = 0;//Max queue size
+	private $Front       = -1;//Front index value through where query will be inserted in queue
+	private $Rear        = -1;//Rear value through where query will be deleted
+	private $QueueIndex  = -1;//Index of the lates item which is replaced
+	
 	private function __construct() {
-		slef :: $QueueSize = MAX_CACHE_SIZE;
+		slef::$QueueSize = MAX_CACHE_SIZE;
 	}
+	
 	public static function getQueryQueueInstance() {
 		static $queryQueue_object = NULL;
 		if( $queryQueue_object == NULL ) {
@@ -18,19 +21,23 @@ class QueryQueue {
 		}
 		return $queryQueue_object;
 	}
-	public function inserInQueue( $query ) {
-		if( self::$front == self::$rear == -1 ) {
-			self::$front += 1;
-			self::$rear  += 1;
+	
+	public function insertInQueue( $query ) {
+		if( self::$Front == self::$Rear == self::$Rear -1 ) {
+			self::$Front += 1;
+			self::$Rear  += 1;
+			self::$QueueIndex  += 1;
 		}
-		$index = 0;
-		if( self::$front >= self::$QueueSize ) {
-			self::$QuesueArray[$index] = $query;
+		if( self::$Front >= self::$QueueSize ) {
+			self::$QuesueArray[ self::$QueueIndex ] = $query;
+			self::$QueueIndex += 1;
+			if( self::$QueueIndex >= self::$QueueSize ) {
+				self::$QueueIndex = 0;
+			}
 		}
 		else {
-			self::$front += 1;
-			self::$QuesueArray[self::$front] = $query;	
-			$index = self::$front;
+			self::$QuesueArray[self::$Front] = $query;	
+			self::$Front += 1;
 		}
 		return $index;
 	}
